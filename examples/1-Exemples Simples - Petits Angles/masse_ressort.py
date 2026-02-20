ba6afd47851c2208f717b9f9e659625af3c65a2b
ALaffargueSOCOFER <a.laffargue@socofer.com>
2026-02-20 12:04:44 +0100

    retravail exemple pour CIR

--- examples/1-Exemples Simples - Petits Angles/masse_ressort.py
+++ examples/1-Exemples Simples - Petits Angles/masse_ressort.py
@@ -12,6 +12,7 @@ from MultiBodySimulation.MBSMechanicalJoint import MBSLinkLinearSpringDamper
 from MultiBodySimulation.MBSMechanicalSystem import MBSLinearSystem
 
 M = 1.0
+J = 1
 k = 20
 c = 1.5
 
@@ -33,40 +34,38 @@ r = solve_ivp(lambda t,z : Amatrix.dot(z) + bvec,
 z_scipy = r.y[0]
 
 
-# Solving multibody tools
+ref_position = np.array([0,0,zref])
+initial_body_position = np.array([0,0,zinit])
+g_vector = np.array([0,0,-9.81])
 
+# Solving with multibody tools
 # Création du système
 mecha_sys = MBSLinearSystem()
-mecha_sys.gravity = np.array([0,0,-9.81])
-
-
-refBody = MBSReferenceBody3D("Reference")
-massBody = MBSRigidBody3D("Mass",M,0.1)
-
-ref_position = np.array([0,0,zref])
-
+# Ajouter la gravité -9.81 en z
+mecha_sys.gravity = g_vector
+# Corps de référence = immobile
+refBody = MBSReferenceBody3D("Ground")
+# Corps simulé
+massBody = MBSRigidBody3D("Mass",M,J)
 # Placer les corps dans le repère global
-refBody.SetReferencePosition(ref_position)
 massBody.SetReferencePosition(ref_position)
-
-joint = MBSLinkLinearSpringDamper(refBody,
+# Créer le ressort-amortisseur
+joint = MBSLinkLinearSpringDamper(refBody, # Corps 1
             ref_position, #Point d'attache au corps 1 dans le repère global
-            massBody,
+            massBody, # Corps 2
             ref_position, #Point d'attache au corps 2 dans le repère global
-            k,
-            c,
-                            )
-
+            k, # Raideur
+            c,) # Amortissement
+# Assembler le système
 mecha_sys.AddRigidBody(refBody)
 mecha_sys.AddRigidBody(massBody)
 mecha_sys.AddLinkage(joint)
-
 # On repositionne le corps dans le repère global pour initialiser la simulation
-massBody.ChangeInitialPosition(np.array([0,0,zinit]))
-
+massBody.ChangeInitialPosition(initial_body_position)
 #Simulation
 t_mbs, results = mecha_sys.RunDynamicSimulation(t_span=t_span,dt=dt)
-z_result = results["Mass"].positions[2]
+#Récupérer les résultats
+z_result = results["Mass"].positions[2] # Indice 2 = direction Z
 
 plt.figure()
 plt.plot(t_eval, z_scipy, label = "Scipy", color = "blue", lw=2)
